c1005a460e4b62573f56082201c3c7378c7de44b
QuesoCaliente <brianc.contacto@gmail.com>
2024-05-30 01:06:57 -0400

    fix declare window

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare global {
-  interface Window {
-    ytube: boolean;
-  }
+interface Window {
+  ytube: boolean;
 }
