f92285d31e14ea84ac6aa5da56f3fc87ca5e4d5c
QuesoCaliente <brianc.contacto@gmail.com>
2024-05-30 02:14:07 -0400

    remove astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import db from "@astrojs/db";
-import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 import { seoConfig } from "./src/utils/seoConfig";
 
@@ -10,8 +8,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), db(), auth(), mdx()],
+  integrations: [tailwind(), icon(), mdx()],
   output: "server",
   adapter: vercel(),
-  site: seoConfig.baseURL
+  site: seoConfig.baseURL,
 });
